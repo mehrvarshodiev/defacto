aecf74a8e66cf71a1d7c76bf0330c3d104986b18
Mehrvar Shodiev <mehrvarshodiev2406@gmail.com>
2026-08-31 02:06:58 +0500

    build: complete Redux Toolkit store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,2 @@
-import {configureStore} from '@reduxjs/toolkit';
-import cart from './slices/cartSlice';
-import filters from './slices/filterSlice';
-import checkout from './slices/checkoutSlice';
-export const store=configureStore({reducer:{cart,filters,checkout}});
-export type RootState=ReturnType<typeof store.getState>;
-export type AppDispatch=typeof store.dispatch;
+import {configureStore} from '@reduxjs/toolkit'; import cart from './slices/cartSlice'; import filters from './slices/filterSlice'; import checkout from './slices/checkoutSlice'; import products from './slices/productSlice'; import wishlist from './slices/wishlistSlice'; import orders from './slices/orderSlice'; import user from './slices/userSlice';
+export const store=configureStore({reducer:{cart,filters,checkout,products,wishlist,orders,user}}); export type RootState=ReturnType<typeof store.getState>; export type AppDispatch=typeof store.dispatch;
