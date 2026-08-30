4c7449ef975f6ce3bb205b5857f68d703ca31a33
Mehrvar Shodiev <mehrvarshodiev2406@gmail.com>
2026-08-31 02:04:20 +0500

    build: add Redux Toolkit store

---
+++ src/store/store.ts
@@ -0,0 +1,2 @@
+import {configureStore} from '@reduxjs/toolkit'; import cart from './slices/cartSlice'; import filters from './slices/filterSlice';
+export const store=configureStore({reducer:{cart,filters}}); export type RootState=ReturnType<typeof store.getState>; export type AppDispatch=typeof store.dispatch;
