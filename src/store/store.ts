07a9af9fe3a6a7bc88812aef5f02eb674d73117a
Mehrvar Shodiev <mehrvarshodiev2406@gmail.com>
2026-08-31 02:05:19 +0500

    build: connect checkout slice to Redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,2 +1,7 @@
-import {configureStore} from '@reduxjs/toolkit'; import cart from './slices/cartSlice'; import filters from './slices/filterSlice';
-export const store=configureStore({reducer:{cart,filters}}); export type RootState=ReturnType<typeof store.getState>; export type AppDispatch=typeof store.dispatch;
+import {configureStore} from '@reduxjs/toolkit';
+import cart from './slices/cartSlice';
+import filters from './slices/filterSlice';
+import checkout from './slices/checkoutSlice';
+export const store=configureStore({reducer:{cart,filters,checkout}});
+export type RootState=ReturnType<typeof store.getState>;
+export type AppDispatch=typeof store.dispatch;
